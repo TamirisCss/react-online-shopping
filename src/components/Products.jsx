4442f88a626a0bbad80ad6405e412b2e470cc965
Tamiris Cristine <44439100+TamirisCss@users.noreply.github.com>
2022-04-30 16:53:42 +0200

    fetch fake API

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Products = () => {
-    return(
-        <div>hello</div>
-    )
-}
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    axios({
+      method: "GET",
+      url: "https://fakestoreapi.com/products",
+    })
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data);
+      })
+      .catch((e) => console.log(e))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return <div>hello</div>;
+};
 
 export default Products;
