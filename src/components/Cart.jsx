9d2d08c135be93b6f903b948691d4a0fbf473b05
Tamiris Cristine <44439100+TamirisCss@users.noreply.github.com>
2022-05-02 20:35:06 +0200

    created file components

---
+++ src/components/Cart.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Cart = () => {
+  return <div>Cart page, nothing here yet :(</div>;
+};
+
+export default Cart;
---
+++ src/components/Favorites.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Favorites = () => {
+  return <div>Favorites Page, nothing here :(</div>;
+};
+
+export default Favorites;
