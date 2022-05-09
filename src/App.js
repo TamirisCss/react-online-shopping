796bfc9b79366ba0b7bda0d02e5e28dee38663d2
Ariane Crestani <ariane.mrv@gmail.com>
2022-05-09 18:37:40 +0200

    refactored file router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import "./style.css/Products.css";
-import Navbar from "./components/Navbar";
-import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
-import Products from "./components/Products";
-import ProductDetails from "./components/ProductDetails";
-import Favorites from "./components/Favorites";
-import Cart from "./components/Cart";
+import Overview from "./Overview"
 
 const App = () => {
     return (
         <div className="App">
-            <Router>
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<Products />}></Route>
-                    <Route
-                        path="/productDetails/:id"
-                        element={<ProductDetails />}
-                    ></Route>
-                    <Route path="/favorites" element={<Favorites />}></Route>
-                    <Route path="/cart" element={<Cart />}></Route>
-                </Routes>
-            </Router>
+           <Overview/>
         </div>
     );
 };
---
+++ src/Overview.js
@@ -0,0 +1,25 @@
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import Products from "./components/Products";
+import ProductDetails from "./components/ProductDetails";
+import Favorites from "./components/Favorites";
+import Cart from './components/Cart'
+import Navbar from './components/Navbar'
+
+export default function Overview() {
+    return (
+        <>
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Products />}></Route>
+                <Route
+                    path="/productDetails/:id"
+                    element={<ProductDetails />}
+                ></Route>
+                <Route path="/favorites" element={<Favorites />}></Route>
+                <Route path="/cart" element={<Cart />}></Route>
+            </Routes>
+        </Router>
+        </>
+    );
+}
