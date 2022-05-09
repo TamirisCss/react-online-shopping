912d392206692ed5b06119494938b2ba8f590740
Tamiris Cristine <44439100+TamirisCss@users.noreply.github.com>
2022-05-09 16:20:03 +0200

    paste code from Overwiew file to fix router error

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import "./style.css/Products.css";
 import Navbar from "./components/Navbar";
-import Overview from "./Overview";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import Products from "./components/Products";
+import ProductDetails from "./components/ProductDetails";
+import Favorites from "./components/Favorites";
+import Cart from "./components/Cart";
 
 const App = () => {
     return (
         <div className="App">
-            <Navbar />
-            <Overview />
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Products />}></Route>
+                    <Route
+                        path="/productDetails/:id"
+                        element={<ProductDetails />}
+                    ></Route>
+                    <Route path="/favorites" element={<Favorites />}></Route>
+                    <Route path="/cart" element={<Cart />}></Route>
+                </Routes>
+            </Router>
         </div>
     );
 };
