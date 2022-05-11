54eecef86d5468d6fcab563c39e410aab158d57c
Tamiris Cristine <44439100+TamirisCss@users.noreply.github.com>
2022-05-11 20:56:25 +0200

    added onClick function to add product to the cart

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,48 +5,55 @@ import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import { useState, useEffect } from "react";
 
+import { useContext } from "react";
+import CartContext from "../CartContext";
+
 const Card = ({ id, image, price, title, category }) => {
-    const card = { id, image, price, title, category };
-    const [favorite, setFavorite] = useState([]);
-    //criar um novo array e set
+  // const card = { id, image, price, title, category };
+  const [favorite, setFavorite] = useState([]);
+  //criar um novo array e set
 
-    useEffect(() => {
-        // storing
-        localStorage.setItem("favorite", JSON.stringify(favorite));
-    }, [favorite]);
+  const { addToCart } = useContext(CartContext);
 
-    useEffect(() => {
-        const favorite = JSON.parse(localStorage.getItem("favorite"));
-        if (favorite) {
-            setFavorite(favorite);
-        }
-    }, []);
+  useEffect(() => {
+    // storing
+    localStorage.setItem("favorite", JSON.stringify(favorite));
+  }, [favorite]);
 
+  useEffect(() => {
+    const favorite = JSON.parse(localStorage.getItem("favorite"));
+    if (favorite) {
+      setFavorite(favorite);
+    }
+  }, []);
 
-    return (
-        <div key={id} className="productItem">
-            <div className="iconsContainer">
-                <IconButton onClick={addFavorite} aria-label="add to favorites">
-                    <FavoriteBorderIcon />
-                </IconButton>
+  return (
+    <div key={id} className="productItem">
+      <div className="iconsContainer">
+        <IconButton /*onClick={addFavorite}*/ aria-label="add to favorites">
+          <FavoriteBorderIcon />
+        </IconButton>
 
-                <IconButton aria-label="add to cart">
-                    <AddShoppingCartIcon />
-                </IconButton>
-            </div>
-            <div className="imageContainer">
-                <img src={image} alt="#" className="imageProduct " />
-            </div>
-            <div className="card-info">
-                <h6>{title}</h6>
-                <h6>{`Price: ${price}`}</h6>
-                <h6>{`Category: ${category}`}</h6>
-            </div>
-            <Link to={`/productDetails/${id}`}>
-                <Button size="small">More details</Button>
-            </Link>
-        </div>
-    );
+        <IconButton
+          aria-label="add to cart"
+          onClick={() => addToCart(image, title, price)}
+        >
+          <AddShoppingCartIcon />
+        </IconButton>
+      </div>
+      <div className="imageContainer">
+        <img src={image} alt="#" className="imageProduct" />
+      </div>
+      <div className="card-info">
+        <h6>{title}</h6>
+        <h6>{`Price: $${price}`}</h6>
+        <h6>{`Category: ${category}`}</h6>
+      </div>
+      <Link to={`/productDetails/${id}`}>
+        <Button size="small">More details</Button>
+      </Link>
+    </div>
+  );
 };
 
 export default Card;
