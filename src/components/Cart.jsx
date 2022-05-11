cafc7afd0fbc41fae64d6b1daa4322c37b99f537
Tamiris Cristine <44439100+TamirisCss@users.noreply.github.com>
2022-05-11 20:57:52 +0200

    updated the cart UI and style the image size

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import "../style.css/Cart.css";
+import CartContext from "../CartContext";
+import { useContext } from "react";
 
 const Cart = () => {
-  return <div>Cart page, nothing here yet :(</div>;
+  const { cartItems } = useContext(CartContext);
+  return (
+    <div>
+      {!cartItems ? (
+        <h1>your cart is empty</h1>
+      ) : (
+        cartItems.map((item) => (
+          <div>
+            <img className="cart-image" src={item.image} alt={item.title} />
+            <p>{item.title}</p>
+            <p>${item.price}</p>
+          </div>
+        ))
+      )}
+    </div>
+  );
 };
 
 export default Cart;
