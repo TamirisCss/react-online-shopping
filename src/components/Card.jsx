65e18c5ef7a4bfeadddfc8a7ce871a792801cc40
Ariane Crestani <ariane.mrv@gmail.com>
2022-05-12 16:46:26 +0200

    add favorite item

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Button, CardActions } from "@mui/material";
+import { Button } from "@mui/material";
 import IconButton from "@mui/material/IconButton";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
@@ -9,51 +9,67 @@ import { useContext } from "react";
 import CartContext from "../CartContext";
 
 const Card = ({ id, image, price, title, category }) => {
-  // const card = { id, image, price, title, category };
-  const [favorite, setFavorite] = useState([]);
-  //criar um novo array e set
+    const card = { id, image, price, title, category };
+    const [favorite, setFavorite] = useState([]);
+    //   //criar um novo array e set
 
-  const { addToCart } = useContext(CartContext);
+    const { addToCart } = useContext(CartContext);
 
-  useEffect(() => {
-    // storing
-    localStorage.setItem("favorite", JSON.stringify(favorite));
-  }, [favorite]);
+    useEffect(() => {
+        // storing
+        localStorage.setItem("favorite", JSON.stringify(favorite));
+    }, [favorite]);
 
-  useEffect(() => {
-    const favorite = JSON.parse(localStorage.getItem("favorite"));
-    if (favorite) {
-      setFavorite(favorite);
-    }
-  }, []);
+    useEffect(() => {
+        const favorite = JSON.parse(localStorage.getItem("favorite"));
+        if (favorite) {
+            setFavorite(favorite);
+        }
+    }, []);
 
-  return (
-    <div key={id} className="productItem">
-      <div className="iconsContainer">
-        <IconButton /*onClick={addFavorite}*/ aria-label="add to favorites">
-          <FavoriteBorderIcon />
-        </IconButton>
+    //favorite nao pode ser usado para modificar, somente setFavorite
+    const addFavorite = () => {
+        for (let i = 0; i < favorite.length; i++) {
+            const item = favorite[i];
+            if (item.id === card.id) {
+                favorite.splice(i);
+                setFavorite(favorite);
+                console.log("tirei o item", card.id, favorite);
+                return;
+            }
+        }
+        const newFavorites = [...favorite, card];
+        setFavorite(newFavorites);
+        console.log("adicionei o item", card.id, newFavorites);
+    };
 
-        <IconButton
-          aria-label="add to cart"
-          onClick={() => addToCart(image, title, price)}
-        >
-          <AddShoppingCartIcon />
-        </IconButton>
-      </div>
-      <div className="imageContainer">
-        <img src={image} alt="#" className="imageProduct" />
-      </div>
-      <div className="card-info">
-        <h6>{title}</h6>
-        <h6>{`Price: $${price}`}</h6>
-        <h6>{`Category: ${category}`}</h6>
-      </div>
-      <Link to={`/productDetails/${id}`}>
-        <Button size="small">More details</Button>
-      </Link>
-    </div>
-  );
+    return (
+        <div key={id} className="productItem">
+            <div className="iconsContainer">
+                <IconButton onClick={addFavorite} aria-label="add to favorites">
+                    <FavoriteBorderIcon />
+                </IconButton>
+
+                <IconButton
+                    aria-label="add to cart"
+                    onClick={() => addToCart(image, title, price)}
+                >
+                    <AddShoppingCartIcon />
+                </IconButton>
+            </div>
+            <div className="imageContainer">
+                <img src={image} alt="#" className="imageProduct " />
+            </div>
+            <div className="card-info">
+                <h6>{title}</h6>
+                <h6>{`Price: ${price}`}</h6>
+                <h6>{`Category: ${category}`}</h6>
+            </div>
+            <Link to={`/productDetails/${id}`}>
+                <Button size="small">More details</Button>
+            </Link>
+        </div>
+    );
 };
 
 export default Card;
