896673eac90cb6180c30ff680c049907b0370ec5
Ari <ariane.mrv@gmail.com>
2022-06-19 23:06:45 +0200

    add material ui for productdetail

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,28 +1,46 @@
 import React from "react";
 import FavoriteContext from "../FavoriteContext";
 import { useContext } from "react";
-import Card from './Card'
+import FavouriteCard from "./Card";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
 import emptyFav from "../images/empty-fav-img.png";
+import Box from "@mui/material/Box";
 
 
 const Favorites = () => {
-  
-    const { favoriteItems } = useContext(FavoriteContext);
- 
-    return (
+  const { favoriteItems } = useContext(FavoriteContext);
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      {favoriteItems.length === 0 ? (
         <div>
-            {favoriteItems.length === 0 ? (
-                <div>
-                    <h2>Your favorite is empty</h2>
-                    <img src={emptyFav} alt="favorite empty" />
-                </div>
-                
-            ) : (favoriteItems.map(({ id, title, image, price, category }) => (
-                <div><Card id={id} title={title} image={image} price={price} category={category}/></div>
-              ))
-            )}
+          <h2>Your favorite is empty</h2>
+          <img src={emptyFav} alt="favorite empty" />
         </div>
-    );
+      ) : (
+        favoriteItems.map(({ id, title, image, price, category }) => (
+          <Card sx={{ width: 275, margin: 2 }}>
+            <CardContent>
+              <FavouriteCard
+                id={id}
+                title={title}
+                image={image}
+                price={price}
+                category={category}
+              />
+            </CardContent>
+          </Card>
+        ))
+      )}
+    </Box>
+  );
 };
 
 export default Favorites;
