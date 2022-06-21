9061b2eb1f3b9c7e1856d610cc0961c9dc85b0a0
Tamiris Cristine <44439100+TamirisCss@users.noreply.github.com>
2022-06-21 10:24:34 +0200

    created Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
+
+import ariAvatar from "../images/ari-avatar.jpeg";
+import tamiAvatar from "../images/tami-avatar.jpg";
+
+const Footer = () => {
+  return (
+    <div>
+      <Box
+        sx={{
+          bgcolor: "#cfe8fc",
+          height: "20vh",
+          width: "100%",
+          marginTop: "2rem",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        
+        <Stack direction="row" spacing={10}>
+          <Avatar alt="Ariane Cretani" src={ariAvatar} />
+          <Avatar alt="Ariane Cretani" src={tamiAvatar} />
+        </Stack>
+      </Box>
+    </div>
+  );
+};
+
+export default Footer;
