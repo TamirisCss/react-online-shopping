baf42827f1c7e04c103bf610048b4f4c583574a6
Ari <ariane.mrv@gmail.com>
2022-07-05 18:15:27 +0200

    remove

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import { margin } from "@mui/system";
+
 
 const SearchBar = ({ onChange }) => {
   return (
@@ -21,7 +21,7 @@ const SearchBar = ({ onChange }) => {
           width: 500,
           maxWidth: "100%",
           color: "#651fff",
-      
+
           borderColor: "#651fff",
           background: "#F8F8FF",
         }}
@@ -32,7 +32,6 @@ const SearchBar = ({ onChange }) => {
           label="FilterSearch"
           id="Filter"
           onChange={onChange}
-          borderRadius="10px"
         />
       </Box>
     </div>
