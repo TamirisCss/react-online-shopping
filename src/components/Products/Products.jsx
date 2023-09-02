f2975d840489af41ab4c019b00c31602c666c8c2
Tamiris Cristine <tamiris.css@gmail.com>
2023-09-02 18:41:40 +0200

    feat: shuffle Array product list

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -15,10 +15,24 @@ const Products = () => {
     );
   };
 
+  const shuffleArray = (array) => {
+    const shuffledArray = [...array];
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i],
+      ];
+    }
+    return shuffledArray;
+  };
+
+  const shuffledData = shuffleArray(data);
+
   return (
     <S.ProductsContainer>
       {loading && <CircularProgress />}
-      {data
+      {shuffledData
         .filter(filterSearch)
         .map(({ id, image, title, price, category }) => (
           <div key={id}>
