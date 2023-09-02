4da6c2e16bf00b39ef392958fd4788b35d21a2d8
Tamiris Cristine <tamiris.css@gmail.com>
2023-09-02 18:58:59 +0200

    fix styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,48 +3,24 @@ import React from "react";
 import ariAvatar from "../../images/ari-avatar.jpeg";
 import tamiAvatar from "../../images/tami-avatar.jpg";
 
+import * as S from "./styles";
+
 const Footer = () => {
   return (
-    <div className="footer">
-      <div
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          gap: "1rem",
-          background: "#b388ff",
-
-          bottom: "0",
-          position: "relative",
-          padding: "80px",
-        }}
-      >
-        <div
-          direction="column"
-          spacing={1}
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <img alt="Ariane Cretani" src={ariAvatar} />
-          <h6>Ariane Crestani</h6>
-        </div>
-        <div
-          direction="column"
-          spacing={1}
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
+    <S.FooterContainer>
+      <S.ProfileContainer>
+        <S.ImgContainer>
           <img alt="Ariane Cretani" src={tamiAvatar} />
-          <h6>Tamiris Cristine</h6>
-        </div>
-      </div>
-    </div>
+        </S.ImgContainer>
+        <p>Tamiris Cristine</p>
+      </S.ProfileContainer>
+      <S.ProfileContainer>
+        <S.ImgContainer>
+          <img alt="Ariane Cretani" src={ariAvatar} />
+        </S.ImgContainer>
+        <p>Ariane Crestani</p>
+      </S.ProfileContainer>
+    </S.FooterContainer>
   );
 };
 
